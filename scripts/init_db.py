50893b304264908b08c844c023ac7d259c039986
Andy Kuny <andrew.kuny@gsa.gov>
2023-12-12 13:52:35 -0500

    Add script to init db

---
+++ scripts/init_db.py
@@ -0,0 +1,20 @@
+import os
+from sqlalchemy import create_engine
+from nad_ch.infrastructure.database import ModelBase, DataProviderModel
+from nad_ch.config import DATABASE_URL
+
+
+def init_db():
+    engine = create_engine(DATABASE_URL)
+
+    # Check if the database file already exists
+    if os.path.exists(DATABASE_URL.split('///')[1]):
+        print('Database already exists.')
+    else:
+        # Create all tables
+        ModelBase.metadata.create_all(engine)
+        print('Database initialized and tables created.')
+
+
+if __name__ == '__main__':
+    init_db()
