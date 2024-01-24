6def4e5dc3ff94db482f76d26a6cc97d5b9e616e
Andy Kuny <andrew.kuny@gsa.gov>
2024-01-24 13:25:12 -0500

    Update env naming convention

--- nad_ch/config/__init__.py
+++ nad_ch/config/__init__.py
@@ -1,7 +1,7 @@
 from .base import APP_ENV
 
 
-if APP_ENV == 'development-local' or APP_ENV == 'test':
+if APP_ENV == 'development_local' or APP_ENV == 'test':
     from .development_local import *
-elif APP_ENV == 'development-remote':
+elif APP_ENV == 'development_remote':
     from .development_remote import *
