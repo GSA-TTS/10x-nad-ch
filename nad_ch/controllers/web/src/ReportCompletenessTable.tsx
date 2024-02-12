5cb9f7a810cc251eec217744741f2315cf7d8880
Andy Kuny <andrew.kuny@gsa.gov>
2024-02-12 12:48:27 -0500

    Working on individual report view

--- nad_ch/controllers/web/src/ReportCompletenessTable.tsx
+++ nad_ch/controllers/web/src/ReportCompletenessTable.tsx
@@ -82,28 +82,76 @@ function Row(props: RowProps) {
   );
 }
 
-export class ReportCompletenessTable extends Component {
+type ReportCompletenessTableState = {
+  isGrouped: boolean;
+};
+
+export class ReportCompletenessTable extends Component<
+  {},
+  ReportCompletenessTableState
+> {
+  constructor() {
+    super();
+    this.state = {
+      isGrouped: false,
+    };
+
+    this.toggleGrouped = this.toggleGrouped.bind(this);
+  }
+
+  toggleGrouped() {
+    this.setState((prevState) => ({
+      isGrouped: !prevState.isGrouped,
+    }));
+  }
+
   render() {
+    console.log(this.state);
     const rows = window.completenessReportData.features
       .sort((a: RowProps, b: RowProps) => b.populated_count - a.populated_count)
       .map((obj: any) => (
         <Row key={obj.provided_feature_name} {...(obj as RowProps)} />
       ));
 
+    const buttonClass = this.state.isGrouped
+      ? "usa-button usa-button-toggle-on"
+      : "usa-button usa-button-toggle-off";
+    const buttonText = this.state.isGrouped
+      ? "Grouped By Status"
+      : "Group By Status";
+
     return (
-      <div class="usa-table-default">
-        <table class="usa-table usa-table--borderless width-full">
-          <thead class="width-full">
-            <tr>
-              <th scope="col">Submitted Field</th>
-              <th scope="col">NAD Field</th>
-              <th scope="col">Status</th>
-              <th scope="col">Empty</th>
-              <th scope="col">Populated</th>
-            </tr>
-          </thead>
-          <tbody>{rows}</tbody>
-        </table>
+      <div class="padding-top-2">
+        <ul class="usa-button-group" style="justify-content: space-between;">
+          <li class="usa-button-group__item">
+            <button
+              type="button"
+              class={buttonClass}
+              onClick={this.toggleGrouped}
+            >
+              {buttonText}
+            </button>
+          </li>
+          <li>
+            <button type="button" class="usa-button">
+              Download .gdb
+            </button>
+          </li>
+        </ul>
+        <div class="usa-table-default">
+          <table class="usa-table usa-table--borderless width-full">
+            <thead class="width-full">
+              <tr>
+                <th scope="col">Submitted Field</th>
+                <th scope="col">NAD Field</th>
+                <th scope="col">Status</th>
+                <th scope="col">Empty</th>
+                <th scope="col">Populated</th>
+              </tr>
+            </thead>
+            <tbody>{rows}</tbody>
+          </table>
+        </div>
       </div>
     );
   }
