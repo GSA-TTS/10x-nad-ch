e1c0c3f3e124a7577213655bd8cdbaf8c43f28d3
Andy Kuny <andrew.kuny@gsa.gov>
2024-03-13 11:14:35 -0400

    Work on create mapping form

--- nad_ch/controllers/web/routes/mappings.py
+++ nad_ch/controllers/web/routes/mappings.py
@@ -1,4 +1,15 @@
-from flask import Blueprint, current_app, render_template, g, request, abort
+import os
+from flask import (
+    Blueprint,
+    current_app,
+    render_template,
+    g,
+    request,
+    abort,
+    redirect,
+    flash,
+    url_for,
+)
 
 # from flask_login import login_required
 
@@ -25,3 +36,23 @@ def create():
 
     title = request.args.get("title")
     return render_template("mappings/create.html", title=title)
+
+
+@mappings_bp.route("/mappings", methods=["POST"])
+def store():
+    if "mapping-csv-input" not in request.files:
+        flash("No file included")
+        return redirect(request.url)
+    file = request.files["mapping-csv-input"]
+    if file.filename == "":
+        flash("No selected file")
+        return redirect(request.url)
+    if file:
+        # Save the mapping
+
+        # TODO implement use case and infrastructure
+        # title = request.form.get("title")
+        # view_model = use_case(g.ctx, title, file)
+
+        # Show the saved mapping
+        return redirect(url_for("mappings.show"))
