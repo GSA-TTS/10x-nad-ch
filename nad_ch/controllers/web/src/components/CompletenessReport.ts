de6e4df02199633c92904f1115c8dbe7e29691ce
Andy Kuny <andrew.kuny@gsa.gov>
2024-03-28 09:27:12 -0400

    Group features by status

--- nad_ch/controllers/web/src/components/CompletenessReport.ts
+++ nad_ch/controllers/web/src/components/CompletenessReport.ts
@@ -1,13 +1,42 @@
 import { AlpineComponent } from 'alpinejs';
 import { BASE_URL } from '../config';
 
+type CompletenessReport = {
+  features: Feature[];
+  overview: Overview;
+};
+
+type Overview = {
+  data_update_required: boolean;
+  etl_update_required: boolean;
+  feature_count: number;
+  features_flagged: number;
+};
+
+type Feature = {
+  provided_feature_name: string;
+  nad_feature_name: string;
+  populated_count: number;
+  null_count: number;
+  required: boolean;
+  status: string;
+  populated_percentage: string;
+  null_percentage: string;
+};
+
+type GroupedFeature = {
+  status: string;
+  features: Feature[];
+};
+
 export interface CompletenessReportComponent {
   id: number;
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  report: any;
+  report: CompletenessReport | null;
+  groupedFeatures: GroupedFeature[];
   isGroupedByStatus: boolean;
   buttonText: string;
   toggleButtonClass: string;
+  groupFeatures(features: Feature[]): GroupedFeature[];
   getStatusTagClass(status: string): string;
 }
 
@@ -16,13 +45,35 @@ export function CompletenessReport(
 ): AlpineComponent<CompletenessReportComponent> {
   return {
     id: parseInt(id),
-    report: [],
+    report: null,
+    groupedFeatures: [],
     isGroupedByStatus: false,
     async init(): Promise<void> {
       const response = await fetch(`${BASE_URL}/api/reports/${this.id}`);
       const reportData = await response.json();
       this.report = JSON.parse(reportData);
+      if (this.report) {
+        this.groupedFeatures = this.groupFeatures(this.report.features);
+      }
       console.log(this.report);
+      console.log(this.groupedFeatures);
+    },
+    groupFeatures(features: Feature[]): GroupedFeature[] {
+      const groupedByStatus = features.reduce<Record<string, Feature[]>>(
+        (acc, obj) => {
+          if (!acc[obj.status]) {
+            acc[obj.status] = [];
+          }
+          acc[obj.status].push(obj);
+          return acc;
+        },
+        {},
+      );
+
+      return Object.keys(groupedByStatus).map((status) => ({
+        status: status,
+        features: groupedByStatus[status],
+      }));
     },
     get buttonText(): string {
       return this.isGroupedByStatus ? 'Ungroup by Status' : 'Group by Status';
