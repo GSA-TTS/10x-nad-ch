e86c8668b57d156fcc5eaaa416394229e3e70d78
Andy Kuny <andrew.kuny@gsa.gov>
2024-03-28 13:10:56 -0400

    Update dev seed script to add report

--- scripts/seed.py
+++ scripts/seed.py
@@ -112,7 +112,161 @@ def main():
 
     filename = DataSubmission.generate_filename(zipped_gdb_path, saved_producer)
     ctx.storage.upload(zipped_gdb_path, filename)
-    new_submission = DataSubmission(filename, saved_producer, saved_column_map)
+    report = {
+        "overview": {
+            "feature_count": 1141,
+            "features_flagged": 0,
+            "etl_update_required": False,
+            "data_update_required": False
+        },
+        "features": [
+            {
+                "provided_feature_name": "AddNum_Pre",
+                "nad_feature_name": "AddNum_Pre",
+                "populated_count": 1141,
+                "null_count": 0,
+                "required": False,
+                "status": "No error"
+            },
+            {
+            "provided_feature_name": "Add_Number",
+            "nad_feature_name": "Add_Number",
+            "populated_count": 1141,
+            "null_count": 0,
+            "required": True,
+            "status": "No error"
+            },
+            {
+            "provided_feature_name": "AddNum_Suf",
+            "nad_feature_name": "AddNum_Suf",
+            "populated_count": 1141,
+            "null_count": 0,
+            "required": False,
+            "status": "No error"
+            },
+            {
+            "provided_feature_name": "AddNo_Full",
+            "nad_feature_name": "AddNo_Full",
+            "populated_count": 1136,
+            "null_count": 5,
+            "required": True,
+            "status": "Rejected"
+            },
+            {
+            "provided_feature_name": "St_PreMod",
+            "nad_feature_name": "St_PreMod",
+            "populated_count": 604,
+            "null_count": 537,
+            "required": False,
+            "status": "Custom ETL needed"
+            },
+            {
+            "provided_feature_name": "St_Name",
+            "nad_feature_name": "St_Name",
+            "populated_count": 1141,
+            "null_count": 0,
+            "required": True,
+            "status": "No error"
+            },
+            {
+            "provided_feature_name": "StNam_Full",
+            "nad_feature_name": "StNam_Full",
+            "populated_count": 1141,
+            "null_count": 0,
+            "required": True,
+            "status": "No error"
+            },
+            {
+            "provided_feature_name": "County",
+            "nad_feature_name": "County",
+            "populated_count": 1140,
+            "null_count": 1,
+            "required": True,
+            "status": "Rejected"
+            },
+            {
+            "provided_feature_name": "Inc_Muni",
+            "nad_feature_name": "Inc_Muni",
+            "populated_count": 1141,
+            "null_count": 0,
+            "required": True,
+            "status": "No error"
+            },
+            {
+            "provided_feature_name": "State",
+            "nad_feature_name": "State",
+            "populated_count": 1141,
+            "null_count": 0,
+            "required": True,
+            "status": "No error"
+            },
+            {
+            "provided_feature_name": "UUID",
+            "nad_feature_name": "UUID",
+            "populated_count": 1141,
+            "null_count": 0,
+            "required": True,
+            "status": "No error"
+            },
+            {
+            "provided_feature_name": "Longitude",
+            "nad_feature_name": "Longitude",
+            "populated_count": 1141,
+            "null_count": 0,
+            "required": True,
+            "status": "No error"
+            },
+            {
+            "provided_feature_name": "Latitude",
+            "nad_feature_name": "Latitude",
+            "populated_count": 1141,
+            "null_count": 0,
+            "required": True,
+            "status": "No error"
+            },
+            {
+            "provided_feature_name": "NatGrid",
+            "nad_feature_name": "NatGrid",
+            "populated_count": 1130,
+            "null_count": 11,
+            "required": True,
+            "status": "Updated by calculation"
+            },
+            {
+            "provided_feature_name": "AddrPoint",
+            "nad_feature_name": "AddrPoint",
+            "populated_count": 1141,
+            "null_count": 0,
+            "required": True,
+            "status": "No error"
+            },
+            {
+            "provided_feature_name": "DateUpdate",
+            "nad_feature_name": "DateUpdate",
+            "populated_count": 1141,
+            "null_count": 0,
+            "required": True,
+            "status": "No error"
+            },
+            {
+            "provided_feature_name": "NAD_Source",
+            "nad_feature_name": "NAD_Source",
+            "populated_count": 0,
+            "null_count": 1141,
+            "required": True,
+            "status": "Rejected"
+            },
+            {
+            "provided_feature_name": "DataSet_ID",
+            "nad_feature_name": "DataSet_ID",
+            "populated_count": 0,
+            "null_count": 1141,
+            "required": True,
+            "status": "Rejected"
+            }
+        ]
+    }
+    new_submission = DataSubmission(filename, saved_producer, saved_column_map, report)
     ctx.submissions.add(new_submission)
 
     os.remove(zipped_gdb_path)
