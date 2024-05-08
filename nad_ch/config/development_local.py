63b932b48574ea671d625738807c054582de04eb
Andy Kuny <andrew.kuny@gsa.gov>
2024-05-08 09:49:16 -0400

    Fix error in config

--- nad_ch/config/development_local.py
+++ nad_ch/config/development_local.py
@@ -46,7 +46,7 @@ class DevLocalApplicationContext(ApplicationContext):
         self._submissions = self.create_submission_repository()
         self._users = self.create_user_repository()
         self._column_maps = self.create_column_map_repository()
-        self.roles = self.create_role_repository()
+        self._roles = self.create_role_repository()
         self._logger = self.create_logger()
         self._storage = self.create_storage()
         self._task_queue = self.create_task_queue()
